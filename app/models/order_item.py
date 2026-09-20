edde6afbe204be7789c3c0c418b629f32bc8ec18
kosionyia <onyiakosisochi@gmail.com>
2026-09-20 22:00:54 +0100

    Rename OrderItems class to OrderItem for consistency and clarity

--- app/models/order_item.py
+++ app/models/order_item.py
@@ -3,14 +3,10 @@ from sqlmodel import SQLModel, Field
 from app.schemas.order import GarmentType
 
 
-<<<<<<< HEAD
 class OrderItems(SQLModel, table=True):
 
     __tablename__ = "Order Items"
 
-=======
-class OrderItem(SQLModel, table=True):
->>>>>>> 80e6c1a7af9f4da3126292be6a0e92e02cb1bdd5
     id: int | None = Field(default=None, primary_key=True)    
     order_id: int = Field(foreign_key="order.id")
     price_list_id: int = Field(foreign_key="price_list.id")
